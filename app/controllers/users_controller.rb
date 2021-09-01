61dcb922589e603809605d10d4a2be0938d89f01
gracejgj <joycelgracej@gmail.com>
2021-09-01 12:47:24 +0800

    users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :set_user, only: [:show, :edit, :update]
+
+  layout 'user'
+
+  def show
+    @user = User.find(params[:id])
+    @listings = Listing.is_available
+    @bookings = @user.bookings
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to user_path(current_user), notice: 'User profile was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:avatar)
+    end
+    #
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+
+    resources :users
+    resources :products, only: [:show] do
+      resources :orders do
+        get 'success'
+        get 'cancel'
+        get 'payment_details'
+      end
+    end
+
+    resources :stripes, only: [:create]
+
+    namespace :platform do
+      get '/home', to: 'pages#host_home'
+
+      resources :dashboards, only: [:show]
+      resources :vendors
+      resources :products do
+        member do
+          delete :delete_photo_attachment
+        end
+      end
+    end
 end
