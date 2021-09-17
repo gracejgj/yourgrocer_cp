82ca8116deb816f3206fddf058aad7da89d2c884
gracejgj <joycelgracej@gmail.com>
2021-09-18 05:09:32 +0800

    commiting Gemfile.lock

---
+++ db/migrate/20210902051132_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.references :vendor, null: false, foreign_key: true
+      t.string :name
+      t.string :description
+      t.float :price_per_quantity
+      t.integer :quantity
+      t.boolean :available, default: false
+
+      t.timestamps
+    end
+  end
+end
